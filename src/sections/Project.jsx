e079065acc27416323b4f42fad434477251f9bed
KIWOLY <ikiwoly@gmail.com>
2026-04-03 12:59:19 +0300

    feat: add setup for the project section

--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 
+const projects = [
+  {
+    title: "e-commerce",
+    description: "",
+    image: "/project/e-commerce.png",
+    tags: ["React", "Django", "Postgress"],
+    link: "#",
+    github: "#"
+  },
+  {
+    title: "Air quality Monitoring",
+    description: "Monitoring Air quality by measuring air polluntant gas",
+    image: "/project/air_quality.png",
+    tags: ["React", "Django", "Postgress", "IOT", "Lorawan", "Influx DB"],
+    link: "#",
+    github: "#"
+  },
+  {
+    title: "e-learning",
+    description: "Platform which facilitate the student on finding the online material",
+    image: "/project/e-learning.png",
+    tags: ["React", "Django", "Postgress"],
+    link: "#",
+    github: "#"
+  }
+
+
+
+
+]
+
+
 const Project = () => {
   return (
-     <section></section>
+    <section></section>
   )
 }
 
